b4d88b6d53ca2c9f592bb698a9d50724ae5d79e1
Bruno Carvalho <comedinhass@gmail.com>
2015-03-29 18:14:32 -0300

    Fixed demonoak and teltime

--- data/actions/scripts/quests/demonoak.lua
+++ data/actions/scripts/quests/demonoak.lua
@@ -2,113 +2,106 @@ local monster1 = {x = 157, y = 109, z = 7}
 local monster2 = {x = 163, y = 110, z = 7}
 local monster3 = {x = 155, y = 116, z = 7}
 local monster4 = {x = 162, y = 116, z = 7}
-local randomizer = math.random(1, 6)
-local tree = getThingfromPos({x = 159, y = 106, z = 7}).uid
 
-function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
-	if itemEx.itemid == 2709 and itemEx.actionid == 7787 and player:getLevel() >= 170 then
-		tree:remove()
-		player:teleportTo(toPosition, true)
+function onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if target.itemid == 2709 and target.actionid == 7787 and player:getLevel() >= 170 then
+		player:teleportTo({x = 159, y = 106, z = 7}, true)
 		player:setStorageValue(8181, 1)
 	else
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 	end
 	
-	if itemEx.itemid == 2709 and itemEx.actionid == 7788 then
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "A player is already doing this quest.")
-	end
-	
-	if itemEx.itemid == 8289 and itemEx.actionid == 8181 and (randomizer == 1) and player:getStorageValue(8181) == 1 then
+	if target.itemid == 8289 and target.actionid == 8181 and (math.random(1, 6) == 1) and player:getStorageValue(8181) == 1 then
 		Game.createMonster("Crypt Shambler", monster1)
 		Game.createMonster("Crypt Shambler", monster2)
 		Game.createMonster("Crypt Shambler", monster3)
 		Game.createMonster("Crypt Shambler", monster4)
-	elseif itemEx.itemid == 8289 and itemEx.actionid == 8181 and (randomizer == 2) and player:getStorageValue(8181) == 1 then
+	elseif target.itemid == 8289 and target.actionid == 8181 and (math.random(1, 6) == 2) and player:getStorageValue(8181) == 1 then
 		Game.createMonster("Bone Beast", monster1)
 		Game.createMonster("Bone Beast", monster2)
 		Game.createMonster("Bone Beast", monster3)
 		Game.createMonster("Bone Beast", monster4)
-	elseif itemEx.itemid == 8289 and itemEx.actionid == 8181 and (randomizer == 3) and player:getStorageValue(8181) == 1 then
+	elseif target.itemid == 8289 and target.actionid == 8181 and (math.random(1, 6) == 3) and player:getStorageValue(8181) == 1 then
 		Game.createMonster("Demon", monster1)
 		Game.createMonster("Demon", monster2)
 		Game.createMonster("Demon", monster3)
 		Game.createMonster("Demon", monster4)
 		player:setStorageValue(8181, -1)
 		player:setStorageValue(8282, 1)
-	elseif itemEx.itemid == 8289 and itemEx.actionid == 8181 and (randomizer >= 4) and player:getStorageValue(8181) == 1 then
+	elseif target.itemid == 8289 and target.actionid == 8181 and (math.random(1, 6) >= 4) and player:getStorageValue(8181) == 1 then
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		player:addHealth(-1000)
 	end
 
-	if itemEx.itemid == 8289 and itemEx.actionid == 8181 and player:getStorageValue(8181) == -1 then
+	if target.itemid == 8289 and target.actionid == 8181 and player:getStorageValue(8181) == -1 then
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 	end
 
-	if itemEx.itemid == 8288 and itemEx.actionid == 8282 and (randomizer == 1) and player:getStorageValue(8282) == 1 then
+	if target.itemid == 8288 and target.actionid == 8282 and (math.random(1, 6) == 1) and player:getStorageValue(8282) == 1 then
 		Game.createMonster("Crypt Shambler", monster1)
 		Game.createMonster("Crypt Shambler", monster2)
 		Game.createMonster("Crypt Shambler", monster3)
 		Game.createMonster("Crypt Shambler", monster4)
-	elseif itemEx.itemid == 8288 and itemEx.actionid == 8282 and (randomizer == 2) and player:getStorageValue(8282) == 1 then
+	elseif target.itemid == 8288 and target.actionid == 8282 and (math.random(1, 6) == 2) and player:getStorageValue(8282) == 1 then
 		Game.createMonster("Giant Spider", monster1)
 		Game.createMonster("Giant Spider", monster4)
-	elseif itemEx.itemid == 8288 and itemEx.actionid == 8282 and (randomizer == 3) and player:getStorageValue(8282) == 1 then
+	elseif target.itemid == 8288 and target.actionid == 8282 and (math.random(1, 6) == 3) and player:getStorageValue(8282) == 1 then
 		Game.createMonster("Blightwalker", monster1)
 		player:setStorageValue(8282, -1)
 		player:setStorageValue(8383, 1)
-	elseif itemEx.itemid == 8288 and itemEx.actionid == 8282 and (randomizer >= 4) and player:getStorageValue(8282) == 1 then
+	elseif target.itemid == 8288 and target.actionid == 8282 and (math.random(1, 6) >= 4) and player:getStorageValue(8282) == 1 then
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		player:addHealth(-1000)
 	end
 
-	if itemEx.itemid == 8288 and itemEx.actionid == 8282 and player:getStorageValue(8282) == -1 then
+	if target.itemid == 8288 and target.actionid == 8282 and player:getStorageValue(8282) == -1 then
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 	end
 
-	if itemEx.itemid == 8290 and itemEx.actionid == 8383 and (randomizer == 1) and player:getStorageValue(8383) == 1 then
+	if target.itemid == 8290 and target.actionid == 8383 and (math.random(1, 6) == 1) and player:getStorageValue(8383) == 1 then
 		Game.createMonster("Crypt Shambler", monster1)
 		Game.createMonster("Crypt Shambler", monster2)
 		Game.createMonster("Crypt Shambler", monster3)
 		Game.createMonster("Crypt Shambler", monster4)
-	elseif itemEx.itemid == 8290 and itemEx.actionid == 8383 and (randomizer == 2) and player:getStorageValue(8383) == 1 then
+	elseif target.itemid == 8290 and target.actionid == 8383 and (math.random(1, 6) == 2) and player:getStorageValue(8383) == 1 then
 		Game.createMonster("Lich", monster1)
 		Game.createMonster("Lich", monster2)
 		Game.createMonster("Lich", monster3)
 		Game.createMonster("Massacre", monster4)
-	elseif itemEx.itemid == 8290 and itemEx.actionid == 8383 and (randomizer == 3) and player:getStorageValue(8383) == 1 then
+	elseif target.itemid == 8290 and target.actionid == 8383 and (math.random(1, 6) == 3) and player:getStorageValue(8383) == 1 then
 		Game.createMonster("Plaguesmith", monster1)
 		player:setStorageValue(8383, -1)
 		player:setStorageValue(8484, 1)
-	elseif itemEx.itemid == 8290 and itemEx.actionid == 8383 and (randomizer >= 4) and player:getStorageValue(8383) == 1 then
+	elseif target.itemid == 8290 and target.actionid == 8383 and (math.random(1, 6) >= 4) and player:getStorageValue(8383) == 1 then
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		player:addHealth(-1000)
 	end
 
-	if itemEx.itemid == 8290 and itemEx.actionid == 8383 and player:getStorageValue(8383) == -1 then
+	if target.itemid == 8290 and target.actionid == 8383 and player:getStorageValue(8383) == -1 then
 		doSendMagicEffect(toPosition, CONST_ME_POFF)
 	end
 
-	if itemEx.itemid == 8291 and itemEx.actionid == 8484 and (randomizer == 1) and player:getStorageValue(8484) == 1 then
+	if target.itemid == 8291 and target.actionid == 8484 and (math.random(1, 6) == 1) and player:getStorageValue(8484) == 1 then
 		Game.createMonster("Hellgorak", monster1)
 		Game.createMonster("Crypt Shambler", monster2)
 		Game.createMonster("Crypt Shambler", monster3)
 		Game.createMonster("Crypt Shambler", monster4)
-	elseif itemEx.itemid == 8291 and itemEx.actionid == 8484 and (randomizer == 2) and player:getStorageValue(8484) == 1 then
+	elseif target.itemid == 8291 and target.actionid == 8484 and (math.random(1, 6) == 2) and player:getStorageValue(8484) == 1 then
 		Game.createMonster("Lich", monster1)
 		Game.createMonster("Lich", monster2)
 		Game.createMonster("Giant Spider", monster2)
 		Game.createMonster("Giant Spider", monster3)
-	elseif itemEx.itemid == 8291 and itemEx.actionid == 8484 and (randomizer == 3) and player:getStorageValue(8484) == 1 then
+	elseif target.itemid == 8291 and target.actionid == 8484 and (math.random(1, 6) == 3) and player:getStorageValue(8484) == 1 then
 		Game.createMonster("Ushuriel", monster1)
 		Game.createMonster("Juggernaut", monster2)
 		player:setStorageValue(8484, -1)
 		player:setStorageValue(8585, 1)
-	elseif itemEx.itemid == 8291 and itemEx.actionid == 8484 and (randomizer >= 4) and player:getStorageValue(8484) == 1 then
+	elseif target.itemid == 8291 and target.actionid == 8484 and (math.random(1, 6) >= 4) and player:getStorageValue(8484) == 1 then
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		player:addHealth(-1000)
 	end
 	
-	if itemEx.itemid == 8291 and itemEx.actionid == 8484 and player:getStorageValue(8484) == -1 then
+	if target.itemid == 8291 and target.actionid == 8484 and player:getStorageValue(8484) == -1 then
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 	end
 	return true
--- data/actions/scripts/quests/teltime.lua
+++ data/actions/scripts/quests/teltime.lua
@@ -5,27 +5,27 @@ local config = {
     timeToRemove = 10
 }
 
-function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
-	local teleport = getTileItemById(config.teleportPos, 1387)
-	local playerPos = player:getPosition()
+function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if item.itemid == 1945 then
-		doCreateTeleport(1387, config.newPos, config.teleportPos)
-		config.teleportPos:sendMagicEffect(CONST_ME_TELEPORT)
-		playerPos:sendMagicEffect(CONST_ME_GIFT_WRAPS)
+		local teleport = Game.createItem(1387, 1, config.teleportPos)
+		teleport:setDestination(config.newPos)
+		Position(config.teleportPos):sendMagicEffect(CONST_ME_TELEPORT)
+		player:getPosition():sendMagicEffect(CONST_ME_GIFT_WRAPS)
 		player:say("The teleport has been created!", TALKTYPE_ORANGE_1)
-		addEvent(doRemoveTeleport, config.timeToRemove * 1000)
+		addEvent(doRemoveTeltime, config.timeToRemove * 1000)
+		item:transform(1946)
 	elseif item.itemid == 1946 then
 		player:sendCancelMessage("The teleport was created! If you get in now can only return at the end of the challenge.")
     end
 	return true
 end
 
-function doRemoveTeleport()
-	local teleport = config.teleportPos:getItemById(1387)
-	local Lever = config.LeverPos:getItemById(1946)
-    if teleport.uid > 0 then
-        teleport.uid:remove()
-        config.teleportPos:sendMagicEffect(CONST_ME_POFF)
-        Lever.uid:transform(1945)
-    end
+function doRemoveTeltime()
+	if Tile(config.teleportPos):getItemById(1387):remove() then
+		Position(config.teleportPos):sendMagicEffect(CONST_ME_POFF)
+	end
+	local lever = Tile(config.LeverPos):getItemById(1946)
+	if lever then
+		lever:transform(1945)
+	end
 end 
