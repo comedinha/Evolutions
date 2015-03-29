55592df8095f5748ef0ff454d14f3010b03005a4
Bruno Carvalho <comedinhass@gmail.com>
2015-03-29 08:14:28 -0300

    oops

--- data/movements/scripts/dough.lua
+++ data/movements/scripts/dough.lua
@@ -1,12 +1,12 @@
 function onAddItem(moveitem, tileitem, position)
-	if moveitem.itemid == 2693 then
-		Item(moveitem.uid):transform(2689)
+	if moveitem:getId() == 2693 then
+		moveitem:transform(2689)
 		position:sendMagicEffect(CONST_ME_HITBYFIRE)
-	elseif moveitem.itemid == 6277 then
-		Item(moveitem.uid):transform(2687, 12)
+	elseif moveitem:getId() == 6277 then
+		moveitem:transform(2687, 12)
 		position:sendMagicEffect(CONST_ME_HITBYFIRE)
-	elseif moveitem.itemid == 8846 then
-		Item(moveItem.uid):transform(8847)
+	elseif moveitem:getId() == 8846 then
+		moveitem:transform(8847)
 		position:sendMagicEffect(CONST_ME_HITBYFIRE)
 	end
 	return true
